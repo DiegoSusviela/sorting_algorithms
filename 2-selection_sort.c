58e0c55adb02bc11dbb3674e4aca81136f63d3c3
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-14 09:23:38 -0700

    fixing seg fault

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,11 +8,14 @@ void selection_sort(int *array, size_t size)
 {
 	unsigned int candidato, pos1, pos2;
 
+	if (!array)
+		return;
+
 	for (pos1 = 0; pos1 < size; pos1++)
 	{
 		candidato = pos1;
 		pos2 = pos1 + 1;
-		while (pos2)
+		while (array[pos2])
 		{
 			if (array[pos2] < array[candidato])
 				candidato = pos2;
