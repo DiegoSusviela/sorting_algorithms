689766556b9f55cfb5484b83df0f18e44e49baec
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-15 11:00:23 -0700

    fixing betty

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -7,34 +7,36 @@ void maxi_merge_split(int *B, int iBegin, int iEnd, int *A)
 {
 	int iMiddle;
 
-    if(iEnd - iBegin <= 1)
-        return;
-    iMiddle = (iEnd + iBegin) / 2;
-    maxi_merge_split(A, iBegin, iMiddle, B);
-    maxi_merge_split(A, iMiddle, iEnd, B);
+	if (iEnd - iBegin <= 1)
+		return;
+	iMiddle = (iEnd + iBegin) / 2;
+	maxi_merge_split(A, iBegin, iMiddle, B);
+	maxi_merge_split(A, iMiddle, iEnd, B);
 	printf("Merging...\n");
 	printf("[left]: ");
 	print_array(B + iBegin, iMiddle - iBegin);
 	printf("[right]: ");
 	print_array(B + iMiddle, iEnd - iMiddle);
 	printf("[Done]: ");
-    maxi_merge(B, iBegin, iMiddle, iEnd, A);
+	maxi_merge(B, iBegin, iMiddle, iEnd, A);
 	print_array(A + iBegin, iEnd - iBegin);
 }
 
 void maxi_merge(int *A, int iBegin, int iMiddle, int iEnd, int *B)
 {
-    int i = iBegin, j = iMiddle, k;
- 
-    for (k = iBegin; k < iEnd; k++) {
-        if (i < iMiddle && (j >= iEnd || A[i] <= A[j])) {
-            B[k] = A[i];
-            i = i + 1;
-        } else {
-            B[k] = A[j];
-            j = j + 1;
-        }
-    }
+	int i = iBegin, j = iMiddle, k;
+
+	for (k = iBegin; k < iEnd; k++) {
+		if (i < iMiddle && (j >= iEnd || A[i] <= A[j])) {
+			B[k] = A[i];
+			i = i + 1;
+		}
+		else
+		{
+			B[k] = A[j];
+			j = j + 1;
+		}
+	}
 }
 
 void copy_array(int *A, int iBegin, int iEnd, int *B)
@@ -43,12 +45,12 @@ void copy_array(int *A, int iBegin, int iEnd, int *B)
 
 	for(k = iBegin; k < iEnd; k++)
 		B[k] = A[k];
-	}
+}
 
 void maxi_merge_sort(int *A, int *B, int n)
 {
-    copy_array(A, 0, n, B);
-    maxi_merge_split(B, 0, n, A);
+	copy_array(A, 0, n, B);
+	maxi_merge_split(B, 0, n, A);
 }
 
 /**
