67e060382fd30b394556b504dd67c9567b26fab8
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-15 11:29:59 -0700

    hgjghj

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -107,7 +107,7 @@ void merge_sort(int *array, size_t size)
 {
 	int *b;
 
-	b = malloc(sizeof(array));
+	b = malloc(sizeof(int *) * (size + 1));
 	if (!b || !array || size < 2)
 		return;
 	maxi_merge_sort(array, b, size);
