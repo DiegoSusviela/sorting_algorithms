a68b5d85e546ca775d598e1414fb113a888fabd7
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-15 10:20:03 -0700

    changed prints

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -17,12 +17,12 @@ void maxi_merge_split(int *B, int iBegin, int iEnd, int *A)
     maxi_merge_split(A, iMiddle, iEnd, B);
 	printf("Merging...\n");
 	printf("[left:] ");
-	print_array(A, iMiddle - iBegin);
+	print_array(A + iBegin, iMiddle);
 	printf("[right:] ");
-	print_array(A + iMiddle, iEnd);
+	print_array(A + iMiddle, iEnd - 1);
     maxi_merge(B, iBegin, iMiddle, iEnd, A);
 	printf("[Done:] ");
-	print_array(A, iEnd + iBegin);
+	print_array(A, iEnd);
 }
 
 void maxi_merge(int *A, int iBegin, int iMiddle, int iEnd, int *B)
