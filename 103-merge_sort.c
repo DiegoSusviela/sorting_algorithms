5c737f1b28d76ced9f2ece7a0a3fb2bc58be2119
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-15 10:39:33 -0700

    added border cases

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -68,5 +68,9 @@ void merge_sort(int *array, size_t size)
 	int *b;
 
 	b = malloc(sizeof(array));
+	if (!b || !array || size < 2)
+		return;
 	maxi_merge_sort(array, b, size);
+	fflush(0);
+	free(b);
 }
