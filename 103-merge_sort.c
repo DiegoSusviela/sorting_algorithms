0dfea83e0ba597c1afa25a0848390ee76df52e0f
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-15 10:31:16 -0700

    chaning where to print

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -19,7 +19,7 @@ void maxi_merge_split(int *B, int iBegin, int iEnd, int *A)
 	printf("[left:] ");
 	print_array(A + iBegin, iMiddle - iBegin);
 	printf("[right:] ");
-	print_array(A + iMiddle, iEnd - 1);
+	print_array(B + iMiddle, iEnd - iMiddle);
 	printf("[Done:] ");
     maxi_merge(B, iBegin, iMiddle, iEnd, A);
 	print_array(A, iEnd);
