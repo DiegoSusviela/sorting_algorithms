9b20be482cd3116523ed9a0ac8e6df052922f9ce
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-15 07:38:17 -0700

    added an esit condition

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -3,6 +3,15 @@
 #include "fun_aux2.c"
 #include "fun_aux3.c"
 
+/**
+* swapper - counts the length of gap_exp string
+* @final: alkdsnasjkld
+* @array: alkdsnasjkld
+* @gap: alkdsnasjkld
+* ---------------------------------
+* Return: the length of the string
+*/
+
 void swapper(int gap, int *array, int final)
 {
 	int i, temp, j;
@@ -82,6 +91,8 @@ void shell_sort(int *array, size_t size)
 	int final = size - 1;
 	int gap = 1;
 
+	if (!array || size < 2)
+		return (NULL);
 	while ((3*gap) + 1 <= final)
 	{
 		gap = (3*gap) + 1;
